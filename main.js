4fb2f2a41b4ebcb3faa5f0a128672c51664d0c03
Dennis Duka <GreyTeddy@users.noreply.github.com>
2020-02-17 19:59:06 +0000

    clean up and arrow functions

--- main.js
+++ main.js
@@ -2,126 +2,126 @@ var gWidth = 500;
 var gHeight = 700;
 var game;
 
-function setup(){
-  createCanvas(gWidth,gHeight);
-	textSize(32);  
-  noStroke();
-  background(255);
-  fill(0);
-	
+function setup() {
+	createCanvas(gWidth, gHeight);
+	textSize(32);
+	noStroke();
+	background(255);
+	fill(0);
+
 	game = new Game();
 	game.setup();
 	textAlign(CENTER);
 }
 
-function draw(){
+function draw() {
 	game.run();
 }
 
-function Game(){
+function Game() {
 	this.ball = new Ball();
 	this.pipes;
 	this.playing = false;
 	this.points = 0;
-	this.setup = function(){
+	this.setup = () => {
 		this.pipes = Array();
-  	this.pipes.push(new Pipe());
+		this.pipes.push(new Pipe());
 		this.pipes.push(new Pipe());
 		this.pipes[1].x = 775;
 		this.playing = true;
 	}
-	this.run = function(){
-		if(this.playing){
+	this.run = () => {
+		if (this.playing) {
 			background(255);
 			this.ball.run();
-			for(var p=0;p<this.pipes.length;p++){
+			for (var p = 0; p < this.pipes.length; p++) {
 				this.pipes[p].run(this.ball);
-				this.pipes[p].touched(this.ball.x,this.ball.y); 
-				this.pipes[p].passed(this.ball.x,this.ball.y);
+				this.pipes[p].touched(this.ball.x, this.ball.y);
+				this.pipes[p].passed(this.ball.x, this.ball.y);
 			}
 		}
-		else{
-			text(">__<",250,100);
+		else {
+			text(">__<", 250, 100);
 		}
 	}
 }
 
-function Ball(){
-  this.name = "ball";
-  this.size = 100; //diameter
-  this.x = 130;
-  this.y = 200;
-  this.vel = 0;
-  this.gravity = 0.4;
-  this.physics = function(){
-    //change y for gravity
-    this.vel += this.gravity;
-    this.y += this.vel;
-  }
-  this.jump = function(){
-    if(this.vel>-10){this.vel = -7;}
-  }
-  this.show = function(){
-//		fill(255,0,0);
-    ellipse(this.x,this.y,this.size/2)
-  }
-  
-  this.run = function(){
-    this.physics();
-    this.show();
-  }
+function Ball() {
+	this.name = "ball";
+	this.size = 100; //diameter
+	this.x = 130;
+	this.y = 200;
+	this.vel = 0;
+	this.gravity = 0.4;
+	this.physics = () => {
+		//change y for gravity
+		this.vel += this.gravity;
+		this.y += this.vel;
+	}
+	this.jump = () => {
+		if (this.vel > -10) { this.vel = -7; }
+	}
+	this.show = () => {
+		//		fill(255,0,0);
+		ellipse(this.x, this.y, this.size / 2)
+	}
+
+	this.run = () => {
+		this.physics();
+		this.show();
+	}
 }
 
-function Pipe(){
-  this.x = gWidth;
-  this.y = map(Math.random(),0,1,100,500);
+function Pipe() {
+	this.x = gWidth;
+	this.y = map(Math.random(), 0, 1, 100, 500);
 	this.ySpace = 80;
 	this.notPassed = true;
-  this.show = function(){
+	this.show = () => {
 		fill(0);
-    rect(this.x,-this.ySpace/2,
-				 50,this.y-this.ySpace/2);
-    rect(this.x,this.y+this.ySpace,
-				 50,gHeight);
-		ellipse(this.x+25,
-						this.y-this.ySpace,50);
-		ellipse(this.x+25,   
-						this.y+this.ySpace,50);
+		rect(this.x, -this.ySpace / 2,
+			50, this.y - this.ySpace / 2);
+		rect(this.x, this.y + this.ySpace,
+			50, gHeight);
+		ellipse(this.x + 25,
+			this.y - this.ySpace, 50);
+		ellipse(this.x + 25,
+			this.y + this.ySpace, 50);
 		fill(255);
-		text(game.points,this.x+25,this.y);
+		text(game.points, this.x + 25, this.y);
 		fill(0);
-		
-  }
-  this.move = function(){
-    this.x -= 2;
-		if(this.x < -50){
-			this.x = gWidth;
-			this.notPassed = true;    
-		}
-	
-  }
-	this.touched = function(ballX,ballY){
-		//ballX>this.x-25 && ballX<this.x+75 ||
-		if((ballX>this.x-25 && ballX<this.x+75 &&
-			(ballY<this.y-this.ySpace || ballY>this.y+this.ySpace)) ||
-			 dist(this.x+25,this.y-this.ySpace,ballX,ballY) < 50 ||
-			 dist(this.x+25,this.y+this.ySpace,ballX,ballY) < 50 ||
-			 ballY<25 || ballY>675)
-			{
-				console.log("( *)__( *)");
-				game.playing = false};
+
 	}
-	this.passed = function(ballX,ballY){
-		if(this.notPassed && ballX >this.x){
+	this.move = () => {
+		this.x -= 2;
+		if (this.x < -50) {
+			this.x = gWidth;
+			this.notPassed = true;
+		}
+
+	}
+	this.touched = (ballX, ballY) => {
+		//ballX>this.x-25 && ballX<this.x+75 ||
+		if ((ballX > this.x - 25 && ballX < this.x + 75 &&
+			(ballY < this.y - this.ySpace || ballY > this.y + this.ySpace)) ||
+			dist(this.x + 25, this.y - this.ySpace, ballX, ballY) < 50 ||
+			dist(this.x + 25, this.y + this.ySpace, ballX, ballY) < 50 ||
+			ballY < 25 || ballY > 675) {
+			console.log("( *)__( *)");
+			game.playing = false;
+		};
+	}
+	this.passed = (ballX) => {
+		if (this.notPassed && ballX > this.x) {
 			game.points += 1;
 			console.log(game.points);
 			this.notPassed = false;
 		}
 	}
-  this.run = function(){
-    this.show();
-    this.move();
-  }
+	this.run = () => {
+		this.show();
+		this.move();
+	}
 }
 
-function keyPressed() {game.ball.jump();}
+function keyPressed() { game.ball.jump(); }
